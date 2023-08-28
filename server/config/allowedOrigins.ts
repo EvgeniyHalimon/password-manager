37bf91ef9aa454a7f13864a0f793ce4c92a3d8f3
EvgeniyHalimon <halimonevgeniy@gmail.com>
2023-08-28 19:22:52 +0300

    GM-0: updated build cors links

--- server/config/allowedOrigins.ts
+++ server/config/allowedOrigins.ts
@@ -4,6 +4,8 @@ const allowedOrigins = [
   'http://localhost:3500',
   'http://localhost:3000',
   'http://localhost:3210',
+  '*',
+  'https://64ecc88121896e3153b828e4--phenomenal-elf-282ccf.netlify.app',
 ];
 
 export default allowedOrigins;
